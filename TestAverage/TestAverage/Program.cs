b6509f9fc8fc4c2b9ab4171ff529a87408a8211b
James Tisdale <james.tisdale89@gmail.com>
2019-10-17 08:48:59 -0500

    updated with user set # of tests

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestAverage
 {
@@ -6,7 +7,34 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many tests do you need to average?");
+            var totalAmountOfTests = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            List<int> avgList = new List<int>();
+
+            for(int i = 0; i < totalAmountOfTests; i++)
+            {
+                Console.WriteLine($"Enter test score {i + 1}:");
+                avgList.Add(int.Parse(Console.ReadLine()));
+                Console.Clear();
+            }
+
+            int sum = 0;
+            foreach(int num in avgList)
+            {
+                sum += num;
+            }
+
+            int average = sum / totalAmountOfTests;
+            Console.WriteLine($"You entered {totalAmountOfTests} tests." +
+                $" The average score of all of those tests is : {average}.");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Would you like to average more tests? Y/N?");
+            Console.ReadLine();
+           
         }
     }
 }
